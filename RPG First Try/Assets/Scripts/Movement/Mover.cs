b0083304f56f6e74c1cc41c113487e764f474bc6
lstledg <linneastledger@gmail.com>
2020-01-11 00:53:07 +0100

    Progress

---
+++ RPG First Try/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }   
+
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor() //Character click to move
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool  hasHit = Physics.Raycast(ray, out hit);
+        if(hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }
+
+    private void UpdateAnimator() //Animation blender and character speed
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+    }
+}
